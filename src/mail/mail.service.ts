f69b8c672b042e48210cdb19ee04fc8137e039bd
Alejandro Melo <dmelo@unal.edu.co>
2022-06-27 12:48:14 -0500

    Adding logger for the email service
    Changes:
    - Adding the custom logger to the email service

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,11 +1,16 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { CustomLogger } from 'src/common/utils/custom_logger';
 
 @Injectable()
 export class MailService {
-    constructor(private mailerService: MailerService) {}
+    logger: CustomLogger;
+    constructor(private mailerService: MailerService) {
+        this.logger = new CustomLogger(MailService.name);
+    }
 
     async sendMail(to: string, subject: string, text: string) {
+        this.logger.debug(`event=sendMail to=${to} outcome=success type=audit`);
         await this.mailerService.sendMail({
             to,
             subject,
